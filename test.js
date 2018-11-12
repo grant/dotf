62308d2404e3839c384ede5be4a4787ef1772ad9
prprhyt <prprhyt@gmail.com>
2018-11-12 18:58:29 +0900

    fix #1(fix dot file permission)

--- test.js
+++ test.js
@@ -1,15 +1,25 @@
 import test from 'ava';
 import dotf from '.';
-
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
 
 
 // Creates
 test('write', async t => {
   // overwrite data
+  let dotglobalfullpath = path.join(os.homedir(), '.myrc1');
+  let dotlocalfullpath = path.join(__dirname, '.myignore1');
   let dotglobal = dotf('~', 'myrc1'); // Global (~)
   let dotlocal = dotf(__dirname, 'myignore1'); // Local (./)
   var writeGlobal = await dotglobal.write({a: 1});
   var writeLocal = await dotlocal.write({a: 1});
+  if("600"!=(fs.statSync(dotglobalfullpath).mode & parseInt(777, 8)).toString(8)){
+    t.fail();
+  }
+  if("600"!=(fs.statSync(dotlocalfullpath).mode & parseInt(777, 8)).toString(8)){
+    t.fail();
+  }
   t.pass();
   await dotglobal.delete();
   await dotlocal.delete();
