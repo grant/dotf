56799e20a4d409452980a1498bb8175822a65dde
PopGoesTheWza <poppoesthewza@gmail.com>
2020-02-29 20:15:11 +0100

    feat: use `graceful-fs`

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
-import { constants, promises as fsPromises } from 'fs';
+import { constants, promises } from 'graceful-fs';
 import { readFile, writeFile } from 'jsonfile';
 import { homedir } from 'os';
 import { join } from 'path';
+
+const { access, chmod, unlink } = promises;
 
 /**
  * A convenient object to interact with dot files
@@ -20,8 +22,8 @@ export interface Dotfile {
 /**
  * Delete, Read, Write, or Exist Dotfiles
  *
- * @param  {__dirname} dirname The relative dirname
- * @param  {string} name The dotfile name
+ * @param {__dirname} dirname The relative dirname
+ * @param {string} name The dotfile name
  * @return {Dotfile} `delete`, `exists`, `read` and `write` Promises
  */
 const dotf = (dirname: string, name: string): Dotfile => {
@@ -32,19 +34,25 @@ const dotf = (dirname: string, name: string): Dotfile => {
   if (dirname[0] === '~') {
     dirname = homedir();
   }
+
   const fullpath = join(dirname, `.${name}`);
 
   return {
-    delete: () => fsPromises.unlink(fullpath),
-    exists: async () => fsPromises.access(fullpath).then(() => true, () => false),
+    delete: () => unlink(fullpath),
+    exists: async () =>
+      access(fullpath)
+        .then(() => true)
+        .catch(() => false),
     read: () => readFile(fullpath),
-    write: process.platform === 'win32'
-    // if a platform is not Windows(include x64)
-    ? obj => writeFile(fullpath, obj).then(() => obj)
-    : obj => writeFile(fullpath, obj)
-      // same as chmod 600
-      .then(() => fsPromises.chmod(fullpath, constants.S_IRUSR | constants.S_IWUSR))
-      .then(() => obj),
+    write:
+      process.platform === 'win32'
+        ? // if a platform is not Windows(include x64)
+          (obj) => writeFile(fullpath, obj).then(() => obj)
+        : (obj) =>
+            writeFile(fullpath, obj)
+              // same as chmod 600
+              .then(() => chmod(fullpath, constants.S_IRUSR | constants.S_IWUSR))
+              .then(() => obj),
   };
 };
 
